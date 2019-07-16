44d3aacfa27f32fa5597519adb636c937da2e18a
Brandon Franks <bgfranks@Brandons-MacBook-Pro.local>
2019-07-16 18:17:45 -0400

    added completed first react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./Components/Layout/Nav";
+import User from "./Components/Users/User";
+import Alert from "./Components/Layout/Alert";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import NotFound from "./Components/Pages/NotFound";
+import GithubState from "./Context/Github/GithubState";
+import AlertState from "./Context/Alert/AlertState";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Nav />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404: NOT FOUND!</h1>
+      <p className="lead">This is not the page you're look for...</p>
+    </div>
+  );
+};
+
+export default NotFound;
